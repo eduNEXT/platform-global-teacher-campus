f8f44a9017a6b7d342e335fae89e51115565d3de
Andres Espinel <andressespinel@hotmail.com>
2023-08-22 12:37:03 -0500

    feat: added initial models and wrapper files

--- platform_global_teacher_campus/models.py
+++ platform_global_teacher_campus/models.py
@@ -1,3 +1,25 @@
 """
 Database models for platform_global_teacher_campus.
 """
+
+from django.db import models
+
+class CourseCategory(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    #parentCategory = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
+    parentCategory = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class ValidationProcess(models.Model):
+    id = models.AutoField(primary_key=True)
+    courseId = models.IntegerField()
+    categories = models.ManyToManyField(CourseCategory)
+    organizationId = models.IntegerField()
+    currentValidationUserId = models.IntegerField()
+    validationBodyId = models.IntegerField()
+
+    def __str__(self):
+        return f"Validation Process for Course {self.courseid}"
