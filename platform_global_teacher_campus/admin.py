d6f2ab5924a41424c6b8437d5a9683952be80215
Andres Espinel <andressespinel@hotmail.com>
2023-08-23 17:33:36 -0500

    feat: added models to the admin site

--- platform_global_teacher_campus/admin.py
+++ platform_global_teacher_campus/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import ValidationBody
+from .models import ValidationBody, CourseCategory, ValidationProcess, ValidationRejectionReason, ValidationProcessEvent, ValidationRules 
 
 admin.site.register(ValidationBody)
+admin.site.register(CourseCategory)
+admin.site.register(ValidationProcess)
+admin.site.register(ValidationRejectionReason)
+admin.site.register(ValidationProcessEvent)
+admin.site.register(ValidationRules)
