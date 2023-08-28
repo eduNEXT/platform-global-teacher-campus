5e49b05c4bdfd68f981deeeb7b0938ce000d91eb
Juan David Buitrago <juan.buitrago@edunext.co>
2023-08-28 15:54:23 -0500

    feat: add force publish course pipeline

---
+++ platform_global_teacher_campus/filters/pipeline.py
@@ -0,0 +1,34 @@
+"""
+The pipeline module defines custom Filters functions that are used in openedx-filters.
+"""
+from openedx_filters import PipelineStep
+from platform_global_teacher_campus.edxapp_wrapper.force_publish import force_publish
+
+ForcePublishCourseRenderStarted = force_publish()
+
+
+class StopForcePublishCourseRender(PipelineStep):
+    """
+    Stop account settings render process raising RedirectToPage exception.
+
+    Example usage:
+
+    Add the following configurations to your configuration file:
+
+        "OPEN_EDX_FILTERS_CONFIG": {
+            "org.openedx.studio.manages.force_publish.render.started.v1": {
+                "fail_silently": false,
+                "pipeline": [
+                    "platform_global_teacher_campus.filters.pipelines.StopForcePublishCourseRender"
+                ]
+            }
+        },
+    """
+    def run_filter(self, context, *args, **kwargs):  # pylint: disable=arguments-differ
+        """
+        Pipeline step that stop force publish course page.
+        """
+        raise ForcePublishCourseRenderStarted.RedirectToPage(
+            "You can't access to account settings.",
+            redirect_to="",
+        )
