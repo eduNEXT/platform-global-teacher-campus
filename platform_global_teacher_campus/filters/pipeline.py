5f64f94f5adbf43bf10a5319a8a88544d3dc8e92
Juan David Buitrago <93566377+JuanDavidBuitrago@users.noreply.github.com>
2023-08-28 22:40:54 -0500

    feat: add force publish course pipeline (#3)
    * feat: add force publish course pipeline
    
    * fix: fix import errors
    
    * feat: add filter to publish/edit course request
    
    * fix: fix filter_type key

---
+++ platform_global_teacher_campus/filters/pipeline.py
@@ -0,0 +1,62 @@
+"""
+The pipeline module defines custom Filters functions that are used in openedx-filters.
+"""
+from openedx_filters import PipelineStep
+from platform_global_teacher_campus.edxapp_wrapper.force_publish import get_force_publish_course
+
+ForcePublishCourseRenderStarted = get_force_publish_course()
+
+
+class StopForcePublishCourseRender(PipelineStep):
+    """
+    Stop account settings render process raising RedirectToPage exception.
+
+    Example usage:
+
+    Add the following configurations to your configuration file:
+
+        "OPEN_EDX_FILTERS_CONFIG": {
+            "org.openedx.studio.manages.force_publish.render.started.v1": {
+                "fail_silently": false,
+                "pipeline": [
+                    "platform_global_teacher_campus.filters.pipeline.StopForcePublishCourseRender"
+                ]
+            }
+        },
+    """
+    def run_filter(self, context, *args, **kwargs):  # pylint: disable=arguments-differ
+        """
+        Pipeline step that stop force publish course page.
+        """
+        raise ForcePublishCourseRenderStarted.RedirectToPage(
+            "You can't access to account settings.",
+            redirect_to="",
+        )
+
+
+class ModifyRequestToBlockCourse(PipelineStep):
+    """
+    Stop account settings render process raising RedirectToPage exception.
+    Example usage:
+    Add the following configurations to your configuration file:
+        "OPEN_EDX_FILTERS_CONFIG": {
+            "org.openedx.studio.contentstore.modify_usage_key_request.started.v1": {
+                "fail_silently": false,
+                "pipeline": [
+                    "platform_global_teacher_campus.filters.pipeline.ModifyRequestToBlockCourse"
+                ]
+            }
+        },
+    """
+    def run_filter(self, request, course_key, *args, **kwargs):  # pylint: disable=arguments-differ
+        """
+        Pipeline step that stop publish course page.
+        """
+
+        if request.json.get('publish') == 'make_public':
+            request.json['publish'] = None
+
+        course_key = str(course_key)
+
+
+        return request
--- platform_global_teacher_campus/settings/common.py
+++ platform_global_teacher_campus/settings/common.py
@@ -6,3 +6,4 @@ def plugin_settings(settings):
     settings.CVW_COURSES_BACKEND = "platform_global_teacher_campus.edxapp_wrapper.backends.courses_v1"
     settings.USER_MODEL_BACKEND = "platform_global_teacher_campus.edxapp_wrapper.backends.users_v1"
     settings.ORGANIZATIONS_BACKEND = "platform_global_teacher_campus.edxapp_wrapper.backends.organizations_v1"
+    settings.FORCE_PUBLISH_BACKEND = "platform_global_teacher_campus.edxapp_wrapper.backends.force_publish_v1"
