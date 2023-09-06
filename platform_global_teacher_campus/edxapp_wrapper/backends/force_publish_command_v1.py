96ac99a63ab424fe232ee879f6ca1428f6e13bdb
juan <juan.buitrago@edunext.co>
2023-09-05 19:42:04 -0500

    refactor: refactor force publish course

--- platform_global_teacher_campus/edxapp_wrapper/backends/force_publish_command_v1.py
+++ platform_global_teacher_campus/edxapp_wrapper/backends/force_publish_command_v1.py
@@ -2,11 +2,44 @@
 Backend for Force Publish Course Command.
 """
 
-from cms.djangoapps.contentstore.management.commands.force_publish import Command
+from cms.djangoapps.contentstore.management.commands.utils import get_course_versions
+from xmodule.modulestore.split_mongo.split_draft import DraftVersioningModuleStore
+from xmodule.modulestore.django import modulestore
+from xmodule.modulestore.mixed import MixedModuleStore
+from opaque_keys.edx.keys import CourseKey
+
 
 def get_force_publish_course_command():
     """
     Gets force publish course command class from edxapp.
     """
 
-    return Command
+    return DraftVersioningModuleStore
+
+def get_course_versions_branches():
+    """
+    Gets force publish course command class from edxapp.
+    """
+
+    return get_course_versions
+
+def get_modulestore():
+    """
+    Gets the UserAttribute model.
+    """
+
+    return modulestore
+
+def get_course_key():
+    """
+    Gets the CourseKey model.
+    """
+
+    return CourseKey
+
+def get_mixed_module_store():
+    """
+    Gets the CourseKey model.
+    """
+
+    return MixedModuleStore
--- platform_global_teacher_campus/edxapp_wrapper/force_publish_command.py
+++ platform_global_teacher_campus/edxapp_wrapper/force_publish_command.py
@@ -13,3 +13,35 @@ def get_force_publish_course_command():
     backend = import_module(backend_function)
 
     return backend.get_force_publish_course_command()
+
+def get_course_versions_branches():
+    """ Gets get_course_versions from commands utils. """
+
+    backend_function = settings.FORCE_PUBLISH_COMMAND_BACKEND
+    backend = import_module(backend_function)
+
+    return backend.get_course_versions_branches()
+
+def get_modulestore():
+    """ Gets the UserAttribute. """
+
+    backend_function = settings.FORCE_PUBLISH_COMMAND_BACKEND
+    backend = import_module(backend_function)
+
+    return backend.get_modulestore()
+
+def get_course_key():
+    """ Gets the CourseKey. """
+
+    backend_function = settings.FORCE_PUBLISH_COMMAND_BACKEND
+    backend = import_module(backend_function)
+
+    return backend.get_course_key()
+
+def get_mixed_module_store():
+    """ Gets the MixedModuleStore. """
+
+    backend_function = settings.FORCE_PUBLISH_COMMAND_BACKEND
+    backend = import_module(backend_function)
+
+    return backend.get_mixed_module_store()
