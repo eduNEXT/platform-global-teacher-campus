b02b9f4b0bfe156745f7c39fcc573bce71c7c6ca
Luis Felipe Castano <felipe.castano@edunext.co>
2023-09-25 10:36:27 -0500

    fix: flow error

--- platform_global_teacher_campus/api/v1/views.py
+++ platform_global_teacher_campus/api/v1/views.py
@@ -132,7 +132,6 @@ def update_validation_process_state(request, course_id):
             validator_course_access_role.delete()
         
         if new_status in [ValidationProcessEvent.StatusChoices.DRAFT, ValidationProcessEvent.StatusChoices.DISAPPROVED, ValidationProcessEvent.StatusChoices.CANCELLED]:
-            validator_course_access_role = CourseAccessRole.objects.filter(user=request.user, course_id=course_id, org=validation_process.organization.name)
             validator_course_access_role.delete()
 
         serializer.save(validation_process=validation_process)
